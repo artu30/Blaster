31fbeed4228b8169d45cccc7d7feb490f9a2e7ee
artu30 <gonzalez.arturo1991@gmail.com>
2024-08-12 13:35:15 +0200

    Shell physics and sound

--- Source/Blaster/Weapon/Casing.h
+++ Source/Blaster/Weapon/Casing.h
@@ -4,6 +4,8 @@
 #include "GameFramework/Actor.h"
 #include "Casing.generated.h"
 
+class USoundCue;
+
 UCLASS()
 class BLASTER_API ACasing : public AActor
 {
@@ -11,12 +13,21 @@ class BLASTER_API ACasing : public AActor
 	
 public:	
 	ACasing();
-
-private:
-	UPROPERTY(VisibleAnywhere)
-	UStaticMeshComponent* CasignMesh;
 	
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* CasingMesh;
+
+	UPROPERTY(EditAnywhere)
+	float ShellEjectionImpulse;
+
+	UPROPERTY(EditAnywhere)
+	USoundCue* ShellSound;
+	
 };
